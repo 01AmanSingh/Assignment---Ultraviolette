9f8a81ace838a73b6ada01b53a4f15d17962bba1
Aman Singh Sengar <155719409+01AmanSingh@users.noreply.github.com>
2026-02-01 22:59:43 +0530

    Delete test_dash.py

--- test_dash.py
+++
@@ -1,8 +0,0 @@
-from dash import Dash, html
-
-app = Dash(__name__)
-app.layout = html.Div("Dash is working!")
-
-if __name__ == "__main__":
-    print("About to start Dash...")
-    app.run_server(host="127.0.0.1", port=8050, debug=False)
